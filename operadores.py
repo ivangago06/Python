c6b68b0e81a562cdeb2dc89dd0e3cd8cca79fbea
ivangago06 <66133327+ivangago06@users.noreply.github.com>
2023-06-01 17:29:06 -0600

    Clase 02
    Update: Operadores de control

---
+++ operadores.py
@@ -0,0 +1,59 @@
+# CASTEO
+
+numero = 99999999.354654465
+numero = int ("5")
+cadena = "2500"
+numero = int (cadena)
+
+print(type(numero))
+
+# Operadores artimeticos enteros
+
+numero = 8+5 #suma
+numero = 10//5 # división representando el resultado en entero
+numero = 10/5 # división representando el resultado en flotante
+numero = 8-3 #resta
+numero = 2**6 #Potencia
+numero = 100%8 #modulo
+numero = 8*3 #multiplicación
+
+print(numero)
+
+# Binarios
+
+binario = "1101"
+valor = int (binario,2) #enteros
+
+print("Valor correspondiente en entero: ", valor)
+
+# Operadores artimeticos enteros
+
+flotante = 8.3+5.5 #suma
+flotante = 10.5//5.2 # división representando el resultado en entero
+flotante = 10.5/5.7 # división representando el resultado en flotante
+flotante = 8.7-3.9 #resta
+flotante = 2.7**6.9 #Potencia
+flotante = 100.7%8.8 #modulo
+flotante = 8.9*3.1 #multiplicación
+
+print(flotante)
+
+print(5==5.5) #Se puede hacer una comparación con retorno booleano (!=,==)
+
+print(round(5.5)) #función para redondear
+
+print(dir(5.5)) #Función para mostrar las propiedades a usar
+
+
+# Operadores lógicos
+
+print(True + True) # esta operación da como resultado 2, donde Python asume True=1
+print(True and False)
+print(True or False)
+print(not True)
+
+print(3>2)
+print(5<8)
+print(10>=10)
+
+print(1<2!=3) # Operadores subsecuentes
